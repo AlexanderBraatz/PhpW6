b906863ec22ab2eb2f9ddcceddb030038f173a44
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-03 16:06:27 +0100

    finished 02ex4

---
+++ 02/ex4.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function oap($arr){
+    if($arr["age"] > 65){
+        return true;
+    } else {
+    return false;
+    }
+};
+
+$a = [ "name" => "Alice", "age" => 52 ];
+$b = [ "name" => "Bob", "age" => 67 ];
+$c = [ "name" => "Charlie", "age" => 24 ];
+$d = [ "name" => "Bob", "age" => 97 ];
+
+dump(
+    oap($a), // false
+    oap($b), // true
+    oap($c), // false
+    oap($d), // true
+);
