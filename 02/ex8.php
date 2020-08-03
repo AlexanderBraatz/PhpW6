945b9faabf4f5e83185a132e201ec919f5a22ba5
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-03 16:24:54 +0100

    finished 02ex8

---
+++ 02/ex8.php
@@ -0,0 +1,18 @@
+<?php
+//Create a function, even, that takes a number as an argument. The function should return true if the number is even and false if it is odd.
+
+require __DIR__ . "/vendor/autoload.php";
+
+function even($number){
+
+    return $number % 2 === 0;
+    
+    // if ($number % 2 === 0){
+    //     return true;
+    // }else {
+    //     return false;
+    // }
+}
+
+dump(even(1)); // false
+dump(even(2)); // true
